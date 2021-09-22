a3016c01d06a919dfa79aa67aa712ba0f1f5f79d
jadermario3377 <jade.m77@gmail.com>
2021-09-21 21:34:53 -0500

    Se crea calculadora clase del 13/09/21 Ciclo 3

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <calculadora-basica/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculadoraBasica from './components/CalculadoraBasica.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalculadoraBasica
+    
   }
 }
 </script>
---
+++ src/components/CalculadoraBasica.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h1>Calculadora Básica</h1>
+        <input type="number" v-model="n1"/>              <!--entrada de tipo numérica-->
+        <input type="number" v-model="n2"/> 
+        <br>
+        <button @click="suma()">Sumar</button>
+        <button @click="resta()">Restar</button>
+        <button @click="multiplica()">Multiplicar</button>
+        <button @click="divide()">Dividir</button>
+        
+        <br>
+        <span>{{ respuesta }}</span>          <!--muestra respuesta interpolado-->
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "CalculadoraBasica",
+    data:()=>{
+        return {
+            n1: null,                  //Retorna n1
+            n2: null,
+            respuesta: ""              
+
+        }
+    },
+    methods:{
+        suma(){
+            this.respuesta = parseInt(this.n1) + parseInt(this.n2); 
+        },
+
+        resta(){
+            this.respuesta = parseInt(this.n1) - parseInt(this.n2); 
+        },
+
+        multiplica(){
+            this.respuesta = parseInt(this.n1) * parseInt(this.n2); 
+        },
+
+        divide(){
+            this.respuesta = parseInt(this.n1) / parseInt(this.n2); 
+        }
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
