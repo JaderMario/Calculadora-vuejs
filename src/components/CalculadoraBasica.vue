d601f3dd4d264ca7a51f5361d31a24ad90cbe66d
jadermario3377 <jade.m77@gmail.com>
2021-09-25 14:11:24 -0500

    Se hiecieron nuevamente los botones y los inputs con vuetify

--- src/components/CalculadoraBasica.vue
+++ src/components/CalculadoraBasica.vue
@@ -1,13 +1,53 @@
 <template>
     <div>
         <h1>Calculadora Básica</h1>
-        <input type="number" v-model="n1"/>              <!--entrada de tipo numérica-->
-        <input type="number" v-model="n2"/> 
+
+        <v-text-field                                    
+            label="Número 1"
+            :rules="rules"
+            hide-details="auto"
+            v-model="n1"
+        ></v-text-field>                      <!--entrada de tipo numérica 1-->
+
+        <v-text-field                                    
+            label="Número 2"
+            :rules="rules"
+            hide-details="auto"
+            v-model="n2"
+        ></v-text-field>
         <br>
-        <button @click="suma()">Sumar</button>
-        <button @click="resta()">Restar</button>
-        <button @click="multiplica()">Multiplicar</button>
-        <button @click="divide()">Dividir</button>
+
+        <v-btn                                           
+            color="primary"
+            elevation="11"
+            rounded
+            small
+            @click="suma()"
+        >Sumar</v-btn>                                    <!--botón Sumar en Beautify-->   
+
+        <v-btn                                           
+            color="primary"
+            elevation="11"
+            rounded
+            small
+            @click="resta()"
+        >Restar</v-btn>     
+
+        <v-btn                                           
+            color="primary"
+            elevation="11"
+            rounded
+            small
+            @click="multiplica()"
+        >Muliplicar</v-btn> 
+
+        <v-btn                                           
+            color="primary"
+            elevation="11"
+            rounded
+            small
+            @click="divide()"
+        >Dividir</v-btn>   
         
         <br>
         <span>{{ respuesta }}</span>          <!--muestra respuesta interpolado-->
@@ -23,9 +63,12 @@ export default {
         return {
             n1: null,                  //Retorna n1
             n2: null,
-            respuesta: ""              
-
-        }
+            respuesta: "", 
+            rules: [
+                value => !!value || 'Requerido.',
+                value => (value && value.length >= 3) || 'Min 3 caracteres',
+            ],           
+        };
     },
     methods:{
         suma(){
