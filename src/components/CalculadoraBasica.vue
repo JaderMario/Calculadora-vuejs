8d194b6d648f8260af2612ff2151229c5317943b
jadermario3377 <jade.m77@gmail.com>
2021-09-26 10:43:46 -0500

    Se agrega archivo Encabezado al proyecto y se cambian los botones con Viutify, se centra todo

--- src/components/CalculadoraBasica.vue
+++ src/components/CalculadoraBasica.vue
@@ -1,63 +1,83 @@
 <template>
-    <div>
-        <h1>Calculadora Básica</h1>
+    <v-container>
+        <encabezado/>                 <!--introduzco lo que está en Encabezado en la página calculadora-->
+        <v-row>
+            <v-col>
+                <h1 class="text-center">Calculadora Básica</h1>
+            </v-col>
+        </v-row>
 
-        <v-text-field                                    
-            label="Número 1"
-            :rules="rules"
-            hide-details="auto"
-            v-model="n1"
-        ></v-text-field>                      <!--entrada de tipo numérica 1-->
-
-        <v-text-field                                    
-            label="Número 2"
-            :rules="rules"
-            hide-details="auto"
-            v-model="n2"
-        ></v-text-field>
+        <v-row>
+            <v-col>
+                <v-text-field                                    
+                    label="Número 1"
+                    :rules="rules"
+                    hide-details="auto"
+                    v-model="n1"
+                ></v-text-field>                      <!--entrada de tipo numérica 1-->
+            </v-col>
+            <v-col>
+                <v-text-field                                    
+                    label="Número 2"
+                    :rules="rules"
+                    hide-details="auto"
+                    v-model="n2"
+                ></v-text-field>
+            </v-col>
+        </v-row>
         <br>
 
-        <v-btn                                           
-            color="primary"
-            elevation="11"
-            rounded
-            small
-            @click="suma()"
-        >Sumar</v-btn>                                    <!--botón Sumar en Beautify-->   
+        <v-row>
+            <v-col class="text-center">
+                <v-btn                                           
+                    color="primary"
+                    elevation="11"
+                    rounded
+                    small
+                    @click="suma()"
+                >Sumar</v-btn>                             <!--botón Sumar en Beautify-->
 
-        <v-btn                                           
-            color="primary"
-            elevation="11"
-            rounded
-            small
-            @click="resta()"
-        >Restar</v-btn>     
+                <v-btn                                           
+                    color="primary"
+                    elevation="11"
+                    rounded
+                    small
+                    @click="resta()"
+                >Restar</v-btn> 
 
-        <v-btn                                           
-            color="primary"
-            elevation="11"
-            rounded
-            small
-            @click="multiplica()"
-        >Muliplicar</v-btn> 
+                <v-btn                                           
+                    color="primary"
+                    elevation="11"
+                    rounded
+                    small
+                    @click="multiplica()"
+                >Muliplicar</v-btn> 
 
-        <v-btn                                           
-            color="primary"
-            elevation="11"
-            rounded
-            small
-            @click="divide()"
-        >Dividir</v-btn>   
+                <v-btn                                           
+                    color="primary"
+                    elevation="11"
+                    rounded
+                    small
+                    @click="divide()"
+                >Dividir</v-btn>
+            </v-col> 
+        </v-row>
         
         <br>
         <span>{{ respuesta }}</span>          <!--muestra respuesta interpolado-->
 
-    </div>
+    </v-container>
 
 </template>
 
 <script type="text/javascript">    // Se agrega este type a la etiqueta script para poder imprimir texto en pantalla
+
+import Encabezado from './Encabezado.vue';
+
 export default {
+    components: { 
+        Encabezado 
+    },
     name: "CalculadoraBasica",
     data:()=>{
         return {
---
+++ src/components/Encabezado.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <v-row>
+      <v-col>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi nisi
+          optio quia numquam quos molestias! Cumque laborum non eligendi magnam
+          obcaecati! At velit, amet dolor culpa quo id distinctio unde.
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum ut
+          facilis similique, possimus commodi dolorem adipisci accusantium id
+          ullam, esse dicta dolore, voluptatem soluta dignissimos voluptatum
+          amet exercitationem molestias veritatis.
+        </p>
+      </v-col>
+    </v-row>
+  
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
